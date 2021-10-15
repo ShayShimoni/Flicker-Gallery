c400dfc90b40f30539b135fd1ac6422577def068
Shay Shimoni <shai.shimoni@gmail.com>
2021-10-15 17:15:31 +0300

    Add a new photos display fragment.

---
+++ app/src/main/java/shay/s/flickergallery/ui/display/FlickrPhotoDisplayFragment.java
@@ -0,0 +1,28 @@
+package shay.s.flickergallery.ui.display;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import shay.s.flickergallery.R;
+
+
+public class FlickrPhotoDisplayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_flickr_photo_display, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
