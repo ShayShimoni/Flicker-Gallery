6c99edf90974d7c6a0d45f84bebd0f298d8314fe
Shay Shimoni <shai.shimoni@gmail.com>
2021-10-15 17:09:20 +0300

    Implement Parcelable for FlickrPhoto class.

--- app/src/main/java/shay/s/flickergallery/model/FlickrPhoto.java
+++ app/src/main/java/shay/s/flickergallery/model/FlickrPhoto.java
@@ -1,10 +1,13 @@
 package shay.s.flickergallery.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 
 import com.google.gson.annotations.SerializedName;
 
-public class FlickrPhoto {
+public class FlickrPhoto implements Parcelable {
     private String  id;
     private String secret;
     private String server;
@@ -41,5 +44,38 @@ public class FlickrPhoto {
                 ", server='" + server + '\'' +
                 ", urlStr='" + urlStr + '\'' +
                 '}';
+    }
+
+    protected FlickrPhoto(Parcel in) {
+        id = in.readString();
+        secret = in.readString();
+        server = in.readString();
+        urlStr = in.readString();
+    }
+
+    //Parcelable implements
+    public static final Creator<FlickrPhoto> CREATOR = new Creator<FlickrPhoto>() {
+        @Override
+        public FlickrPhoto createFromParcel(Parcel in) {
+            return new FlickrPhoto(in);
+        }
+
+        @Override
+        public FlickrPhoto[] newArray(int size) {
+            return new FlickrPhoto[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(secret);
+        dest.writeString(server);
+        dest.writeString(urlStr);
     }
 }
